df9c739b06a33378a2b39eada1f9eec4700cefe1
JaiDhyani <jaiwithani@gmail.com>
2025-10-07 21:22:26 -0700

    feat: capture raw replay callback payloads

---
+++ config/debug_callback.py
@@ -0,0 +1,5 @@
+"""Proxy module exposing DebugCallback for LiteLLM config import."""
+
+from luthien_proxy.proxy.debug_callback import DebugCallback, debug_callback
+
+__all__ = ["DebugCallback", "debug_callback"]
